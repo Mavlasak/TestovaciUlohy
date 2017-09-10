67394eca161b08cc3d1e93ad0cd18149447a4cc8
TestANT <mavlasak@email.cz>
2017-09-11 00:43:59 +0200

    Přidej komponentu s formulářem

--- Nette/ANT/app/presenters/HomepagePresenter.php
+++ Nette/ANT/app/presenters/HomepagePresenter.php
@@ -2,11 +2,11 @@
 
 namespace App\Presenters;
 
+class HomepagePresenter extends BasePresenter {
 
-class HomepagePresenter extends BasePresenter
-{
-	public function renderDefault()
-	{
-		$this->template->anyVariable = 'any value';
-	}
+    protected function createComponentFormular() {
+        $formular = new \App\Component\formular();
+        return $formular;
+    }
+
 }
